31165f428730dc394d48481f90becbc75cec90b7
Ishwarsing34 <igirase65@gmail.com>
2026-02-08 19:19:41 +0530

    added deployed backend url to avoid cors error

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ import userRouter from './routes/userRoutes.js';
 const app = express();
 const port = process.env.PORT || 4000;
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173','https://mern-auth-eight-xi.vercel.app']
 
 app.use(express.json());
 app.use(cookieParser());
